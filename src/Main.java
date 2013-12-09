417801e9105c651d4aea47a9959cd98638cda87d
Richard Lee <lee7ster@gmail.com>
2013-12-08 18:25:53 -0800

    added writing to memory, generating random new solutions shows overall improvements using the write function, seems to work..

--- src/Main.java
+++ src/Main.java
@@ -10,140 +10,143 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner(new File("input.txt"));
-		
-		//get inputs from input file
+
+		// get inputs from input file
 		HashSet<Node> nodes = new HashSet<Node>();
-		while(sc.hasNextLine()){
+		while (sc.hasNextLine()) {
 			String nextLine = sc.nextLine();
-			if(nextLine.trim().equals("*"))
+			if (nextLine.trim().equals("*"))
 				break;
 			nodes.add(new Node(nextLine.trim()));
 		}
 
 		HashSet<Node> startNodes = new HashSet<Node>();
-		while(sc.hasNextLine()){
+		while (sc.hasNextLine()) {
 			String nextLine = sc.nextLine();
-			if(nextLine.trim().equals("*"))
+			if (nextLine.trim().equals("*"))
 				break;
 			startNodes.add(new Node(nextLine.trim()));
 		}
 
 		HashSet<Node> endNodes = new HashSet<Node>();
-		while(sc.hasNextLine()){
+		while (sc.hasNextLine()) {
 			String nextLine = sc.nextLine();
-			if(nextLine.trim().equals("*"))
+			if (nextLine.trim().equals("*"))
 				break;
 			endNodes.add(new Node(nextLine.trim()));
 		}
-		
+
 		HashMap<Edge, Edge> edges = new HashMap<Edge, Edge>();
-		while(sc.hasNextLine()){
+		while (sc.hasNextLine()) {
 			String nextLine = sc.nextLine();
 			Scanner lineScanner = new Scanner(nextLine);
 			Edge e = new Edge(new Node(lineScanner.next().trim()), new Node(lineScanner.next().trim()), lineScanner.nextInt());
 			edges.put(e, e);
 		}
 
-		//Hashmap that holds the results of Floyd-Warshall's shortest paths algorithm
+		// Hashmap that holds the results of Floyd-Warshall's shortest paths
+		// algorithm
 		HashMap<Edge, Edge> sPathMap = new HashMap<Edge, Edge>();
-		for(Node e1 : nodes)
-			for(Node e2 : nodes)
+		for (Node e1 : nodes)
+			for (Node e2 : nodes)
 				sPathMap.put(new Edge(e1, e2, Integer.MAX_VALUE), new Edge(e1, e2, Integer.MAX_VALUE));
-		for(Node e1 : nodes){
+		for (Node e1 : nodes) {
 			sPathMap.put(new Edge(e1, e1, 0), new Edge(e1, e1, 0));
 		}
-		for(Edge e : edges.keySet()){
+		for (Edge e : edges.keySet()) {
 			sPathMap.put(e, e);
 		}
 
-		for(Node e1 : nodes)
-			for(Node e2 : nodes)
-				for(Node e3 : nodes)
-				{
+		for (Node e1 : nodes)
+			for (Node e2 : nodes)
+				for (Node e3 : nodes) {
 					int directCost = sPathMap.get(new Edge(e2, e3, -1)).getCost();
 					int firstCost = sPathMap.get(new Edge(e2, e1, -1)).getCost();
 					int secCost = sPathMap.get(new Edge(e1, e3, -1)).getCost();
-					//adding 2 ints might give overflow, just skip over it for now by doing this check...
-					if(firstCost == Integer.MAX_VALUE || secCost == Integer.MAX_VALUE)
+					// adding 2 ints might give overflow, just skip over it for
+					// now by doing this check...
+					if (firstCost == Integer.MAX_VALUE || secCost == Integer.MAX_VALUE)
 						continue;
-					if(directCost > (firstCost + secCost)){
+					if (directCost > (firstCost + secCost)) {
 						sPathMap.put(new Edge(e2, e3, (firstCost + secCost)), new Edge(e2, e3, (firstCost + secCost)));
 					}
 				}
 
-//		for(Edge e : sPathMap.values()){
-//			System.out.println(e.getFrom().getName() + " " + e.getTo().getName() + " " + e.getCost());
-//		}
-		
-		
-		
+		// for(Edge e : sPathMap.values()){
+		// System.out.println(e.getFrom().getName() + " " + e.getTo().getName()
+		// + " " + e.getCost());
+		// }
+
 		// l represents how many shared memory objects we have
 		int l = 5;
 		// j represents how many solutions each memory can have
-		int j = 20;
-		
-		//however long we should run the program
-		int secondsToRun = 15;
+		int j = 10;
+
+		// however long we should run the program
+		int secondsToRun = 5;
 		if (args.length >= 2) {
 			l = Integer.parseInt(args[0]);
 			j = Integer.parseInt(args[1]);
 		}
-		//we can optionally accept a third param to run it for longer
-		if(args.length >= 3){
+		// we can optionally accept a third param to run it for longer
+		if (args.length >= 3) {
 			secondsToRun = Integer.parseInt(args[2]);
 		}
 		SharedMemory sharedMem = new SharedMemory(l, j);
 		// fill each Memory with j solutions, also called [ki] in the paper
 		for (int i = 0; i < l; i++) {
 			for (int k = 0; k < j; k++) {
-				if(k > sharedMem.getMemoryArray()[i].getMemCapacity())
+				if (k > sharedMem.getMemoryArray()[i].getMemCapacity())
 					throw new Exception("Created too many solutions...");
-				//here we want to call one of the constructors to create a solution and add it
+				// here we want to call one of the constructors to create a
+				// solution and add it
 				Solution solution = createRandomSolution(startNodes, endNodes, edges);
-
-				//then we write it into the memory index
+				// then we write it into the memory index
 				sharedMem.getMemoryArray()[i].getSolutions().add(solution);
 			}
 		}
+
+		calcAndPrintSharedMemoryCost(sharedMem);
 		
 		Solution bestSol = new Solution();
-		
 		long timeToRunUntil = System.currentTimeMillis() + secondsToRun * 1000;
-		while(System.currentTimeMillis() < timeToRunUntil){
-			//get a random improvement heuristic
-			
-			
+		while (System.currentTimeMillis() < timeToRunUntil) {
+			// get a random improvement heuristic
+
 			int memoryIndex = new Random().nextInt(l);
-			
+
 			Solution solToImprove = sharedMem.getMemoryArray()[memoryIndex].getRandomSolution();
-			
-			//run our heuristic and get a new solution
-			Solution improvedSol = null;
-			//destroy a random by replacing our new solution in here.
+
+			// run our heuristic and get a new solution
+			Solution improvedSol = createRandomSolution(startNodes, endNodes, edges);
+			// destroy a random by replacing our new solution in here.
 			sharedMem.getMemoryArray()[memoryIndex].write(improvedSol);
-			
-			if(improvedSol != null && improvedSol.getTotalCost() < bestSol.getTotalCost()){
+
+			if (improvedSol != null && improvedSol.getTotalCost() < bestSol.getTotalCost()) {
 				bestSol = improvedSol;
 			}
 		}
+		
+		calcAndPrintSharedMemoryCost(sharedMem);
 	}
 
-	//creates a random solution from given edges, sources, and destination (making sure no loops exist)
+	// creates a random solution from given edges, sources, and destination
+	// (making sure no loops exist)
 	private static Solution createRandomSolution(HashSet<Node> startNodes, HashSet<Node> endNodes, HashMap<Edge, Edge> edges) {
 		Solution sol = new Solution();
-		
-		//put end nodes in an array so we can remove each randomly chosen one.
+
+		// put end nodes in an array so we can remove each randomly chosen one.
 		ArrayList<Node> endNodesArr = new ArrayList<Node>();
-		for(Node e : endNodes){
+		for (Node e : endNodes) {
 			endNodesArr.add(e);
 		}
-		
-		//for each source node, choose a random end node and find a path to it.
-		for(Node e : startNodes){
+
+		// for each source node, choose a random end node and find a path to it.
+		for (Node e : startNodes) {
 			int index = new Random().nextInt(endNodesArr.size());
 			Node dest = endNodesArr.get(index);
 			endNodesArr.remove(index);
-			
+
 			Path path = generateRandomPath(e, dest, edges);
 			sol.addPath(path);
 		}
@@ -153,42 +156,51 @@ public class Main {
 
 	private static Path generateRandomPath(Node start, Node dest, HashMap<Edge, Edge> edges) {
 		HashSet<Edge> usedEdges = new HashSet<Edge>();
-		
+
 		Node currentNode = start;
 		Path path = new Path();
 
-		//we want to find the set of edges that go to node we have no yet traverse..
-		while(!currentNode.equals(dest)){
-			//find all the edges we can go to
+		// we want to find the set of edges that go to node we have no yet
+		// traverse..
+		while (!currentNode.equals(dest)) {
+			// find all the edges we can go to
 			ArrayList<Edge> possibleEdges = new ArrayList<Edge>();
-			for(Edge e : edges.keySet())
-				if(e.getFrom().equals(currentNode) || e.getTo().equals(currentNode))
+			for (Edge e : edges.keySet())
+				if (e.getFrom().equals(currentNode) || e.getTo().equals(currentNode))
 					possibleEdges.add(e);
-			
-			//remove edges we already we used, since we don't want cycles
-			for(int i = possibleEdges.size() - 1; i >= 0; i--)
-			{
-				if(usedEdges.contains(possibleEdges.get(i)))
+
+			// remove edges we already we used, since we don't want cycles
+			for (int i = possibleEdges.size() - 1; i >= 0; i--) {
+				if (usedEdges.contains(possibleEdges.get(i)))
 					possibleEdges.remove(i);
 			}
-			
-			//if we have no more moves left, just go back to the start
-			if(possibleEdges.size() == 0 ){
+
+			// if we have no more moves left, just go back to the start
+			if (possibleEdges.size() == 0) {
 				currentNode = start;
 				path = new Path();
 				usedEdges = new HashSet<Edge>();
-			}
-			else{
+			} else {
 				Edge edgeToUse = possibleEdges.get(new Random().nextInt(possibleEdges.size()));
 				path.addEdge(edgeToUse);
-				if(edgeToUse.getFrom().equals(currentNode))
+				if (edgeToUse.getFrom().equals(currentNode))
 					currentNode = edgeToUse.getTo();
 				else
 					currentNode = edgeToUse.getFrom();
 				usedEdges.add(edgeToUse);
 			}
 		}
-		
 		return path;
+	}
+	
+
+	private static void calcAndPrintSharedMemoryCost(SharedMemory sharedMem) {
+		int cost = 0;
+		for (Memory mem : sharedMem.getMemoryArray()) {
+			for (Solution sol : mem.getSolutions()) {
+				cost += sol.getTotalCost();
+			}
+		}
+		System.out.println(cost);
 	}
 }
--- src/Memory.java
+++ src/Memory.java
@@ -24,11 +24,27 @@ public class Memory {
 	}
 
 	public void write(Solution improvedSol) {
+		int nSolCost = improvedSol.getTotalCost();
+		int maxSolCost = Integer.MIN_VALUE;
+		for(Solution e : solutions){
+			if(e.getTotalCost() > maxSolCost)
+				maxSolCost = e.getTotalCost();
+		}
+		
 		// test to see if this solution is worse than worst solution,
-		// if it is, just return
+		// if it is, just return without writing
+		if(nSolCost >= maxSolCost){
+			return;
+		}
 		
 		// choose a random one to replace using roulette rule (higher score => more likely to be removed)
-		
+		ArrayList<Short> indexes = new ArrayList<Short>();
+		for(int i = 0; i < solutions.size(); i++){
+			for(int j = 0; j < solutions.get(i).getTotalCost(); j++)
+				indexes.add((short)i);
+		}
+
 		// replace now
+		solutions.set(indexes.get(new Random().nextInt(indexes.size())), improvedSol);
 	}
 }
